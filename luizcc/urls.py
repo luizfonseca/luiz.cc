dcd41319f2d9045286360042b42d4b620abfd966
Luiz Fonseca <eu@luiz.cc>
2013-06-23 19:59:38 -0300

    Change static url

--- luizcc/urls.py
+++ luizcc/urls.py
@@ -17,4 +17,10 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    
+)
+
+
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
