1939fe68b848d37d2ade974ae0067b907d7e1da3
Luiz Fonseca <eu@luiz.cc>
2013-06-23 15:26:50 -0300

    First commit

---
+++ about/views.py
@@ -0,0 +1,8 @@
+# Create your views here.
+from django.http import HttpResponse
+
+
+
+def index(request):
+  render() 
+
---
+++ luizcc/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+
+from about import views as about
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', about.index, name='index'),
+    
+    # Examples:
+    # url(r'^luizcc/', include('luizcc.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
